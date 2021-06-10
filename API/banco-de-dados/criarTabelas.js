22e084d45368c70881b6ee7d6522f6292aeb1f95
Srta Katsumi <73000702+srtakatsumi@users.noreply.github.com>
2021-06-09 21:33:45 -0300

    Delete API directory

--- API/banco-de-dados/criarTabelas.js
+++
@@ -1,6 +0,0 @@
-const ModeloTabela = require('../rotas/produtos/ModeloTabelaProdutos')
-
-ModeloTabela
-    .sync()
-    .then(() => console.log('Tabela criada com sucesso'))
-    .catch(console.log)
